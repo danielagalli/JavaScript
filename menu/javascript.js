ece35c0488a830d7248ef501482b1e3dfad33ace
danielagalli <dani09gally@gmail.com>
2024-10-22 09:59:32 -0300

    inserindo uma nova pasta Menu

---
+++ menu/javascript.js
@@ -0,0 +1,19 @@
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item)=>
+        item.classList.remove('ativo')
+    )
+}
+menuItem.forEach((item)=>
+    item.addEventListener('click', seleclink)
+)
+
+//Expandir o menu no caso ao clicar em cima dos três risquinhos
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
