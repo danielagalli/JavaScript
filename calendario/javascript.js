e353ba05869132559d5e0c6fe1d05d80d206ae2f
danielagalli <dani09gally@gmail.com>
2024-11-06 01:26:05 -0300

    Salvando um novo projeto

---
+++ calendario/javascript.js
@@ -0,0 +1,6 @@
+let el = document.querySelector('.number-days');
+
+for(i = 1; i <=31; i++){
+        el.innerHTML+='<span>'+i+'</span>';
+    
+}
--- menu/javascript.js
+++ menu/javascript.js
@@ -4,7 +4,10 @@ function selectLink(){
     menuItem.forEach((item)=>
         item.classList.remove('ativo')
     )
+    this.classList.add('ativo')
 }
+
+
 menuItem.forEach((item)=>
     item.addEventListener('click', seleclink)
 )
